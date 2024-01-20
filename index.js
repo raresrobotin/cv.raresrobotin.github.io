476ca9a12662a4b3bfaa9d2db1178a353b3b72b1
Robotin Rares <robotin.r.a@gmail.com>
2024-01-20 11:03:04 +0200

    second version

---
+++ index.js
@@ -0,0 +1,74 @@
+//variabile publice/globale
+var activePage = "skills";
+
+//functii publice
+function $(selector) {
+  console.info("gaseste elementul: %o ", selector);
+
+  var el = document.querySelector(selector);
+  console.info("elementul gasit este:", el);
+  return el;
+}
+
+function hide(id) {
+  console.info("hide", id);
+  document.getElementById(id).style.display = "none";
+}
+
+function show(id) {
+  console.info("show", id);
+  var page = document.getElementById(id);
+  console.info("page", page);
+  page.style.display = "block";
+}
+
+function showPage(id) {
+  console.info("show page", id);
+  var prevLink = $("a[data-page=" + activePage + "]");
+  prevLink.classList.remove("active");
+  hide(activePage);
+
+  var nextLink = $(`a[data-page=${id}]`);
+  nextLink.classList.add("active");
+  show(id);
+  activePage = id;
+}
+
+function initEvents() {
+  var toolbar = $("#top-menu-bar");
+  toolbar.addEventListener("click", function (e) {
+    if (e.target.matches("a")) {
+      var page = e.target.dataset.page;
+      console.warn("event", page);
+      showPage(page);
+    }
+  });
+}
+
+function showSkills() {
+  var ul = document.querySelector("#skills ul");
+
+  var skills = [
+    { name: "HTML", endorcements: 6, favorite: true },
+    { name: "CSS", endorcements: 5 },
+    { name: "JS", endorcements: 7, favorite: true },
+    { name: "Word", endorcements: 1, favorite: false },
+  ];
+
+  var text = skills.map(function (skill) {
+    var cls = "";
+    if (skill.favorite == true) {
+      cls = "favorite";
+    }
+    console.info("%o (%o)", skill.name, cls);
+    return `<li class="${cls}">${skill.name} <span> - ${skill.endorcements}</span></li>`;
+  });
+  console.warn(text);
+
+  ul.innerHTML = text.join("");
+}
+
+//executii
+showSkills();
+showPage(activePage);
+initEvents();
